cdc2417e6d2e6c3e38bed8f199bf6e8f8771f03e
anon <sys@tehnosvar.ru>
2022-06-28 16:08:51 +0300

    edd:

--- interface/src/backend/handler.py
+++ interface/src/backend/handler.py
@@ -1,107 +1,34 @@
-import asyncio
-import time
-import random
+import time, logging
 from PySide2.QtCore import QObject, Signal, Slot, QThread
-
-from src.backend import database
-import logging
-
-
-
-
-class ChartWorker(QObject):
-    """ Поток данных построения таблицы """
-    finished = Signal()
-    chart = Signal(list)
-
-    def __init__(self, parent=None):
-        # super().__init__(parent)
-        super(ChartWorker, self).__init__(parent)
-        self.__finished = False
-
-    count = 0
-
-    @Slot()
-    def doWork(self):
-        while True:
-            if self.__finished:
-                break
-            time.sleep(1)
-            print("Chart worker started")
-            if self.count % 2 == 0:
-                data = [
-                    {"name": "name 1", "x": self.count, "y": random.randint(60, 80)},
-                    {"name": "name 2", "x": self.count, "y": random.randint(40, 80)},
-                    {"name": "name 3", "x": self.count, "y": random.randint(10, 50)},
-                    {"name": "name 4", "x": self.count, "y": random.randint(60, 100)},
-                    {"name": "name 5", "x": self.count, "y": random.randint(120, 160)},
-                ]
-            else:
-                data = [
-                    {"name": "name 1", "x": self.count, "y": random.randint(125, 145)},
-                    {"name": "name 2", "x": self.count, "y": random.randint(100, 160)},
-                ]
-            
-
-            self.count += 1
-            self.chart.emit(data)
-
-    def stop(self):
-        print(f"[-] First worker stopped")
-        self.__finished = True
-
+from .modules import charts, database
 
 
 class Handler(QObject):
-    """ """
+    """ Управление модулями приложения (Обработчик)"""
 
-    list_programs = None
-    new_program = None
-    list_corrector_data = None
-    list_reflow_data = None
-
-
-    # Сигналы
+    # Сигналы  ( Сигналы потоков )
     weldingProgramms = Signal(list)
     correctorData = Signal(list)
     reflowData = Signal(list)
     diametersData = Signal(list)
-
     chartData = Signal(list)
-
     allDataSaved = Signal()
 
-    # Проверка на правильное завершение работы компонентов системы
-    treads_check_list = {
-        "chartView": False,  #   Тред работы заполнения таблицы
-    } 
 
-
-    """
-        Charts 
-    """
+    # Поток построения графиков
     def chart_worker_signal(self, data):
         self.chartData.emit(data)
 
-
     def create_chart_stream(self):
-        """ Создаём поток таблиц """
-
         self.chart_thread = QThread()
-        self.chart_worker = ChartWorker()
+        self.chart_worker = charts.ChartWorker()
         self.chart_worker.moveToThread(self.chart_thread)
-
         self.chart_thread.started.connect(self.chart_worker.doWork)
-
         self.chart_worker.chart.connect(self.chart_worker_signal)
         self.chart_worker.finished.connect(self.chart_thread.quit)
-
-
         self.chart_thread.start()
 
-
     def destroy_chart_stream(self):
-        print(f"[+] Останавливаем поток диаграмм")
         self.chart_worker.stop()
         self.chart_thread.quit()
 
@@ -217,21 +144,12 @@ class Handler(QObject):
 
 
 
-
-
-
-
-
     # Остановка и запуск приложения
-
     @Slot()
     def running_application(self):
         """ Запуск необходимых сервисов при старте """
 
         self.create_chart_stream()
-
-
-
 
 
     @Slot()
@@ -242,18 +160,14 @@ class Handler(QObject):
         self.destroy_chart_stream()
 
 
-
         # Ждём пока поток будет завершён
         while self.chart_thread.isRunning():
+
+            # Проверяем остановлен ли поток
+            print(f"RUNNING FIRST THREAD: { self.chart_thread.isFinished() }")
+
             print("Останавливаем First Worker")
             time.sleep(1)
-
-
-
-        # Проверяем остановлен ли поток
-        print(f"RUNNING FIRST THREAD: { self.chart_thread.isFinished() }")
-
-
 
         # Закрываем приложение, если все потоки остановились
         self.allDataSaved.emit()
---
+++ interface/src/backend/modules/charts.py
@@ -0,0 +1,44 @@
+from PySide2.QtCore import QObject, Slot, Signal
+
+import time, random, logging
+
+
+class ChartWorker(QObject):
+    """ Поток данных построения таблицы """
+    
+    finished = Signal()
+    chart = Signal(list)
+
+    def __init__(self, parent=None):
+        # super().__init__(parent)
+        super(ChartWorker, self).__init__(parent)
+        self.__finished = False
+
+    count = 0
+
+    @Slot()
+    def doWork(self):
+        while True:
+            if self.__finished:
+                break
+            time.sleep(0.5)
+            if self.count % 2 == 0:
+                data = [
+                    {"name": "name 1", "x": self.count, "y": random.randint(60, 80)},
+                    {"name": "name 2", "x": self.count, "y": random.randint(40, 80)},
+                    {"name": "name 3", "x": self.count, "y": random.randint(10, 50)},
+                    {"name": "name 4", "x": self.count, "y": random.randint(60, 100)},
+                    {"name": "name 5", "x": self.count, "y": random.randint(120, 160)},
+                ]
+            else:
+                data = [
+                    {"name": "name 1", "x": self.count, "y": random.randint(125, 145)},
+                    {"name": "name 2", "x": self.count, "y": random.randint(100, 160)},
+                ]
+            
+
+            self.count += 1
+            self.chart.emit(data)
+
+    def stop(self):
+        self.__finished = True
