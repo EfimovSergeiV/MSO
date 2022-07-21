9fb6c66976dae3763e3adeae38a0e582ccc1d0c6
anon <sys@tehnosvar.ru>
2022-07-21 15:28:05 +0300

    add: create example

--- interface/src/backend/modules/database.py
+++ interface/src/backend/modules/database.py
@@ -96,27 +96,9 @@ def create(data):
     conn.commit()
     conn.close()
 
-    # for table in ['programm_correctorparammodel', 'programm_reflowparammodel',]:
-    #     related_field, id = "programm_id", 60
-    #     response = cursor.execute("SELECT * FROM %(table)s WHERE %(related_field)s = %(id)s" % { "table": table, "related_field": related_field, "id": id })
-    #     data = response.fetchone()
-    #     print(table, data)
 
-    # models = {'programm_correctorsectionmodel': 'corrector_id', 'programm_reflowsectionmodel': 'reflow_id'}
-    # for table in models:
-    #     id = 30 #ID корректора или оплавления
-    #     response = cursor.execute("SELECT * FROM %(table)s WHERE %(related_field)s = %(id)s" % { "table": table, "related_field": models[table], "id": id })
-    #     data = response.fetchall()
-    #     print(table, data)
-
-    # for table in ['programm_sedimentpressuresensormodel','programm_primaryvoltagesensormodel','programm_preheatingmodel','programm_positionsensormodel','programm_pkpressuremetersensormodel','programm_otherparametersensormodel','programm_oiltemperaturesensormodel','programm_nkpressuremetersensormodel','programm_hydraulicpressuresensormodel','programm_currentsensormodel','programm_burningmodel','programm_clampmodel',]:
-    #     related_field, id = "programm_id", 60
-    #     response = cursor.execute("SELECT * FROM %(table)s WHERE %(related_field)s = %(id)s" % { "table": table, "related_field": related_field, "id": id })
-    #     data = response.fetchone()
-    #     print(table, data)
-
-    # conn.close()
-
+def update():
+    pass
 
 
 
@@ -390,4 +372,4 @@ data = {
 
 
 
-create(data)
+update(data)
